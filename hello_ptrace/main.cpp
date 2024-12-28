4749f3340e7b342623294039c534ec0e4c79f779
Anzor Apshev <anzor.apshev@gmail.com>
2024-12-28 15:20:08 +0000

    fi

--- hello_ptrace/main.cpp
+++ hello_ptrace/main.cpp
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
+#include <optional>
 
 
 int execChild() {
@@ -19,13 +20,52 @@ int execChild() {
     return 0;
 }
 
-void checkError(long result) {
+auto checkError(auto result) {
     if (result != 0) {
         throw std::runtime_error("Unexpected error: " + std::to_string(result));
     }
+    return result;
 }
 
-void tryPrintSysCallInfo(int childPid, int wstatus) {
+constexpr int SYS_CALL_WRITE = 1;
+constexpr int SYS_STDOUT = 1;
+
+static int lastWriteSize = 0;
+
+void handleConsoleWriteEnter(int childPid, user_regs_struct& registers) {
+    auto dataAddress = registers.rsi;
+    int count = registers.rdx;
+    std::string buffer;
+
+    while (count > 0) {
+        std::cout << "Reading count: " << count << " from address: " << dataAddress << std::endl;
+        errno = 0;
+        std::uint64_t data = ptrace(PTRACE_PEEKDATA, childPid, dataAddress, 0);
+        if (data == -1 && errno != 0) {
+            throw std::runtime_error("Reading memory is failed!");
+        }
+
+        int readCount = std::min(count, 8);
+        count -= readCount;
+        dataAddress += readCount;
+        const auto* begin = reinterpret_cast<char*>(&data);
+        buffer.append(begin, readCount);
+    }
+
+    std::cout << "Intercepted out buffer: " << buffer << std::endl;
+    lastWriteSize = registers.rdx;
+    registers.rdx = 0;
+    registers.rsi = 0;
+    checkError(ptrace(PTRACE_SETREGS, childPid, 0, &registers));
+}
+
+void handleConsoleWriteExit(int childPid, user_regs_struct& registers) {
+    registers.rax = lastWriteSize;
+    lastWriteSize = 0;
+    checkError(ptrace(PTRACE_SETREGS, childPid, 0, &registers));
+}
+
+void tryPrintSysCallInfo(int childPid, int wstatus, std::optional<std::uint64_t>& lastSysCall) {
     int isSysCall = WSTOPSIG(wstatus);
     if (isSysCall && 0x80 == 0) {
         std::cerr << "Is not a syscall" << std::endl;
@@ -34,8 +74,26 @@ void tryPrintSysCallInfo(int childPid, int wstatus) {
 
     user_regs_struct registers; 
     checkError(ptrace(PTRACE_GETREGS, childPid, 0, &registers));
+
+    bool sysExit = lastSysCall == registers.orig_rax;
+
     std::cerr << "syscall no: " << registers.orig_rax
-        << " " << SYS_CALL_MAP.at(registers.orig_rax) << std::endl;
+        << " " << SYS_CALL_MAP.at(registers.orig_rax)
+        << " sysexit: " << sysExit << std::endl;
+
+    if (registers.orig_rax == SYS_CALL_WRITE && registers.rdi == SYS_STDOUT) {
+        if (sysExit) {
+            handleConsoleWriteExit(childPid, registers);
+        } else {
+            handleConsoleWriteEnter(childPid, registers);
+        }
+    }
+
+    if (sysExit) {
+        lastSysCall.reset();
+    } else {
+        lastSysCall = registers.orig_rax;
+    }
 }
 
 int execParent(int childPid) {
@@ -53,9 +111,11 @@ int execParent(int childPid) {
     std::cout << "Waited for initial child stop." << std::endl;
     checkError(ptrace(PTRACE_SETOPTIONS, childPid, 0, PTRACE_O_TRACESYSGOOD));
 
+    std::optional<std::uint64_t> lastSysCall;
+
     while (!WIFSIGNALED(wstatus) && !WIFEXITED(wstatus)) {
         if (WIFSTOPPED(wstatus)) {
-            tryPrintSysCallInfo(childPid, wstatus);
+            tryPrintSysCallInfo(childPid, wstatus, lastSysCall);
             checkError(ptrace(PTRACE_SYSCALL, childPid, 0, 0));
         } else {
             std::cerr << "Status is not stopped nor exited: " << wstatus << std::endl;
